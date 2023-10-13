aabe470e5074e9bffd8babab71e33c24690c9bb2
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-10-13 09:31:04 +0300

    scrapping

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
+import { extractPrice } from "../utils";
 
 
 export async function scrapedAmazonProduct(url:string){
@@ -25,8 +26,22 @@ export async function scrapedAmazonProduct(url:string){
         const sa = cheerio.load(response.data);
 
         const title = sa('#productTitle').text().trim();
+        const currentPrice = extractPrice(
+            sa('.priceToPay span.a-price-whole'),
+            sa('a.size.base.a-color-price'),
+            sa('.a-button-selected .a-color-base'),
+            sa('.a-price.a-text-price')
+        );
 
-        console.log(title);
+        const originalPrice = extractPrice(
+            sa('#priceblock_ourprice'),
+            sa('.a-price.a-text-price span.a-offscreen'),
+            sa('#listPrice'),
+            sa('#priceblock_dealprice'),
+            sa('.a-size-base.a-color-price')
+        );
+
+        console.log({title,currentPrice});
 
     } catch (error:any) {
         throw new Error(`Failed to scrape product: ${error.message}`)
